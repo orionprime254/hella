35ff739b305afcd0cadb587b84c3d1d374e6c7ea
Nemesis Prime <brianamatalo34@gmail.com>
2023-03-13 15:53:01 +0300

    NExt Step is CRUD operations

--- lib/profile.dart
+++ lib/profile.dart
@@ -12,8 +12,41 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('lib/icons/user.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 20),
+                // Add more UI elements here
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
